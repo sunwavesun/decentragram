c5232be3f2fdb6a475583546482bff5a0ce8b253
joycaleb9705 <joycaleb9705@berkeley.edu>
2021-10-13 15:22:08 -0700

    Added jpgs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,22 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
+        {/* <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
+          // target="_blank"
           rel="noopener noreferrer"
         >
           <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
           Decentragram
-        </a>
+        </a> */}
+        <p
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          Decentragram
+        </p>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
